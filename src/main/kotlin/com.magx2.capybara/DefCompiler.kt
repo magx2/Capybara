d3b072aa6b1ccd4680a9d9e66153e82fa742c804
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2020-10-01 21:34:38 +0200

    #3 Add stub for `DefCompiler.kt`

---
+++ src/main/kotlin/com.magx2.capybara/DefCompiler.kt
@@ -0,0 +1,9 @@
+package com.magx2.capybara
+
+class DefCompiler(private val compilationContext: CompilationContext,
+                  private val compileUnit: CompileUnitWithFlatStructs,
+                  private val fullyQualifiedStructNames: Map<Type, Struct>) {
+    fun def(def: Def) {
+
+    }
+}
