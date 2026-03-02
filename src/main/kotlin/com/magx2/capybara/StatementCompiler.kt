b11b274a5b7bd4ebe2cd95eecd4eeb9106c5fc8f
Martin Grześlowski <martin.grzeslowski@gmail.com>
2026-03-02 11:05:18 +0100

    Purge the old code

--- src/main/kotlin/com/magx2/capybara/StatementCompiler.kt
+++
@@ -1,9 +0,0 @@
-package com.magx2.capybara
-
-class StatementCompiler(private val compilationContext: CompilationContext,
-                        private val compileUnit: CompileUnitWithFlatStructs,
-                        private val fullyQualifiedStructNames: Map<Type, Struct>) {
-    fun statement(statement: Statement) {
-
-    }
-}
--- src/main/kotlin/com/magx2/capybara/export/CapybaraExport.kt
+++
@@ -1,8 +0,0 @@
-package com.magx2.capybara.export
-
-import com.magx2.capybara.export.python.CompileUnitToExport
-
-
-interface CapybaraExport {
-    fun export(units: Set<CompileUnitToExport>)
-}
