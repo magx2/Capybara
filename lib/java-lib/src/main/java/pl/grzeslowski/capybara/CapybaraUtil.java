f41126032d07da9de5907fff62f782baac723241
Martin Grześlowski <martin.grzeslowski@gmail.com>
2026-03-23 21:02:54 +0100

    feat: add CapybaraUtil primitive power overloads

--- lib/java-lib/src/main/java/pl/grzeslowski/capybara/CapybaraUtil.java
+++ lib/java-lib/src/main/java/pl/grzeslowski/capybara/CapybaraUtil.java
@@ -1,18 +1,43 @@
 package pl.grzeslowski.capybara;
 
 public interface CapybaraUtil {
-    // todo write versions for float, double, short, byte
-    static int power(int a, int b) {
+    static int power(int base, int exponent) {
         int result = 1;
 
-        while (b > 0) {
-            if ((b & 1) == 1) {
-                result *= a;
+        while (exponent > 0) {
+            if ((exponent & 1) == 1) {
+                result *= base;
             }
-            a *= a;
-            b >>= 1;
+            base *= base;
+            exponent >>= 1;
         }
 
         return result;
+    }
+
+    static double power(double base, int exponent) {
+        double result = 1;
+
+        while (exponent > 0) {
+            if ((exponent & 1) == 1) {
+                result *= base;
+            }
+            base *= base;
+            exponent >>= 1;
+        }
+
+        return result;
+    }
+
+    static float power(float base, int exponent) {
+        return (float) power((double) base, exponent);
+    }
+
+    static short power(short base, int exponent) {
+        return (short) power((int) base, exponent);
+    }
+
+    static byte power(byte base, int exponent) {
+        return (byte) power((int) base, exponent);
     }
 }
