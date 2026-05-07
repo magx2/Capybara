c76e4ea5f467382d4df882242d66d4033b5bab5b
Martin Grześlowski <martin.grzeslowski@gmail.com>
2026-05-07 08:40:16 +0200

    feat(#105): add reflection_value intrinsic

--- capy/src/e2e-tests/java/dev/capylang/test/ReflectionFeatureTest.java
+++ capy/src/e2e-tests/java/dev/capylang/test/ReflectionFeatureTest.java
@@ -11,12 +11,12 @@ class ReflectionFeatureTest {
         var letter = (Reflection.TypeInfo) ReflectionFeature.reflectionLetter();
 
         assertThat(letter.name()).isEqualTo("Letter");
-        assertThat(letter.fields()).extracting(Reflection.FieldInfo::name).containsExactly("name");
+        assertThat(letter.fields()).extracting(Reflection.DataFieldInfo::name).containsExactly("name");
         assertThat(letter.data()).extracting(Reflection.DataInfo::name).containsExactlyInAnyOrder("A", "B");
 
         var letterFromValue = (Reflection.TypeInfo) ReflectionFeature.reflectionLetter2(new ReflectionFeature.A("letter-a", 1));
         assertThat(letterFromValue.name()).isEqualTo("Letter");
-        assertThat(letterFromValue.fields()).extracting(Reflection.FieldInfo::name).containsExactly("name");
+        assertThat(letterFromValue.fields()).extracting(Reflection.DataFieldInfo::name).containsExactly("name");
         assertThat(letterFromValue.data()).extracting(Reflection.DataInfo::name).containsExactlyInAnyOrder("A", "B");
     }
 
@@ -24,13 +24,13 @@ class ReflectionFeatureTest {
     void reflectsFunctionalDataTypes() {
         var a = (Reflection.DataInfo) ReflectionFeature.reflectionA();
         assertThat(a.name()).isEqualTo("A");
-        assertThat(a.fields()).extracting(Reflection.FieldInfo::name).containsExactly("name", "a");
+        assertThat(a.fields()).extracting(Reflection.DataFieldInfo::name).containsExactly("name", "a");
         assertThat(a.fields().get(1).type()).isInstanceOf(Reflection.PrimitiveInfo.class);
         assertThat(a.fields().get(1).type().name()).isEqualTo("int");
 
         var aFromValue = (Reflection.DataInfo) ReflectionFeature.reflectionA2(new ReflectionFeature.A("letter-a", 1));
         assertThat(aFromValue.name()).isEqualTo("A");
-        assertThat(aFromValue.fields()).extracting(Reflection.FieldInfo::name).containsExactly("name", "a");
+        assertThat(aFromValue.fields()).extracting(Reflection.DataFieldInfo::name).containsExactly("name", "a");
 
         var b = (Reflection.DataInfo) ReflectionFeature.reflectionB();
         var bField = b.fields().stream()
@@ -39,6 +39,16 @@ class ReflectionFeatureTest {
                 .orElseThrow();
         assertThat(bField.type()).isInstanceOf(Reflection.ListInfo.class);
         assertThat(((Reflection.ListInfo) bField.type()).element_type().name()).isEqualTo("string");
+    }
+
+    @Test
+    void reflectsFunctionalDataValues() {
+        var a = ReflectionFeature.reflectionValueA(new ReflectionFeature.A("letter-a", 1));
+
+        assertThat(a.info().name()).isEqualTo("A");
+        assertThat(a.fields()).extracting(Reflection.DataValueFieldInfo::name).containsExactly("name", "a");
+        assertThat(a.fields().get(1).type()).isInstanceOf(Reflection.PrimitiveInfo.class);
+        assertThat(a.fields().get(1).type().name()).isEqualTo("int");
     }
 
     @Test
