2b20621e9b36cb6ffa40760057b5d2d415f6acf2
Martin Grzeslowski <martin@grzeslowski.pl>
2026-05-13 09:47:37 +0200

    feat(#138): derive test timestamps from millis

--- lib/capybara-lib/src/main/java/dev/capylang/test/TestSelection.java
+++ lib/capybara-lib/src/main/java/dev/capylang/test/TestSelection.java
@@ -106,7 +106,6 @@ public final class TestSelection {
                 filteredFiles.add(new CapyTest.TestFile(
                         testFile.file_name(),
                         filteredCases,
-                        testFile.timestamp(),
                         testFile.timestamp_millis()
                 ));
             }
