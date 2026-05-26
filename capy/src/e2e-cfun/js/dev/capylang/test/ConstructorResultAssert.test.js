72afc3ee309f66b94b3b7f3d22c2a1469f7e4e8a
Martin Grzeslowski <martin@grzeslowski.pl>
2026-05-26 02:22:34 +0200

    test(#179): finish Capybara e2e test migration

--- capy/src/e2e-cfun/js/dev/capylang/test/ConstructorResultAssert.test.js
+++
@@ -1,8 +0,0 @@
-const test = require('node:test');
-const { assert, generatedModule } = require('./_support');
-
-test('ConstructorResultAssert', () => {
-    const constructorAssert = generatedModule('dev/capylang/test/ConstructorResultAssert.js');
-    assert.equal(constructorAssert.validDateAssertSucceeds(), true);
-    assert.equal(constructorAssert.invalidDateAssertFails(), true);
-});
--- capy/src/e2e-cfun/js/dev/capylang/test/OptionInterop.test.js
+++
@@ -1,8 +0,0 @@
-const test = require('node:test');
-const { assertNone, assertSome, generatedModule } = require('./_support');
-
-test('OptionInterop', () => {
-    const optionInterop = generatedModule('dev/capylang/test/OptionInterop.js');
-    assertSome(optionInterop.someValue(7), 7);
-    assertNone(optionInterop.noneValue());
-});
