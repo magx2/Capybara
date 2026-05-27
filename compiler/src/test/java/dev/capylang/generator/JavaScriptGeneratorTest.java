0ebd8377af0d15dbead0b04232df2d225bdc8dd9
Martin Grzeslowski <martin@grzeslowski.pl>
2026-05-27 10:50:28 +0200

    fix(#174): move native provider wiring into annotation

--- compiler/src/test/java/dev/capylang/generator/JavaScriptGeneratorTest.java
+++ compiler/src/test/java/dev/capylang/generator/JavaScriptGeneratorTest.java
@@ -1035,6 +1035,15 @@ class JavaScriptGeneratorTest {
         return new RawModule("NativeProvider", "/capy/meta_prog", """
                 annotation NativeProvider on interface {
                     qualifier: String = ""
+                    lifetime: String = "singleton"
+                    javaClassName: String = ""
+                    javaFactory: String = "constructor"
+                    javascriptModule: String = ""
+                    javascriptExport: String = ""
+                    javascriptFactory: String = "new"
+                    pythonModule: String = ""
+                    pythonClassName: String = ""
+                    pythonFactory: String = "call"
                 }
                 """);
     }
--- compiler/src/test/java/dev/capylang/generator/ObjectOrientedJavaGeneratorTest.java
+++ compiler/src/test/java/dev/capylang/generator/ObjectOrientedJavaGeneratorTest.java
@@ -1237,6 +1237,15 @@ class ObjectOrientedJavaGeneratorTest {
         return new RawModule("NativeProvider", "/capy/meta_prog", """
                 annotation NativeProvider on interface {
                     qualifier: String = ""
+                    lifetime: String = "singleton"
+                    javaClassName: String = ""
+                    javaFactory: String = "constructor"
+                    javascriptModule: String = ""
+                    javascriptExport: String = ""
+                    javascriptFactory: String = "new"
+                    pythonModule: String = ""
+                    pythonClassName: String = ""
+                    pythonFactory: String = "call"
                 }
                 """);
     }
