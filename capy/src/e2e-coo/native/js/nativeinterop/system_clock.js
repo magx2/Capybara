dea2a9f395fd71a034907cdfe56c97153375a3d3
Martin Grzeslowski <martin@grzeslowski.pl>
2026-05-28 08:25:13 +0200

    chore(#174): remove stale native e2e fixtures

--- capy/src/e2e-coo/native/js/nativeinterop/system_clock.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-class SystemClock {
-    now_millis() {
-        return 12345;
-    }
-}
-
-module.exports = { SystemClock };
--- capy/src/e2e-coo/native/py/nativeinterop/system_clock.py
+++
@@ -1,3 +0,0 @@
-class SystemClock:
-    def now_millis(self):
-        return 12345
