7084f098ed8279307b187630e0bf7c2074afc815
Martin Grzeslowski <martin@grzeslowski.pl>
2026-06-18 18:04:10 +0200

    fix(#184): remove bootstrap patch workaround

--- bootstrap-patches/capy-0.3.1-signature/src/main/java/dev/capylang/compiler/CompiledGenericTypeParameter.java
+++
@@ -1,8 +0,0 @@
-package dev.capylang.compiler;
-
-public record CompiledGenericTypeParameter(String name) implements CompiledType {
-    @Override
-    public String toString() {
-        return "CompiledGenericTypeParameter[name=" + name + "]";
-    }
-}
